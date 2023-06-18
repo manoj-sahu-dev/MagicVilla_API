f1e5ac21271d36face2572d8275c5e9160fd2fa9
Manoj Sahu <manoj_sahu@outlook.com>
2023-06-18 07:23:54 +0530

    added mapper

---
+++ MagicVilla_VillaAPI/Models/Dto/VillaUpdateDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(30)]
+        public string Name { get; set; }
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
+        public int SqureFeet { get; set; }
+        public int Occupecy { get; set; }
+        public String ImageUrl { get; set; }
+        public String Amenity { get; set; }
+
+    }
+}
+
